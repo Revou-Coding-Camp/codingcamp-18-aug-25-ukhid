da5153dc3cd3c481bc20ef8d4c84909e963614c7
Ukhid <ukhid.syarif@gmail.com>
2025-08-21 21:23:56 +0700

    Refactor input validation and enhance CSS styles for better layout

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ console.log("Hello, Wolrd!");
 
 let listTodo = [];
 
+/// Validate Inputs
 function validateForm() {
     /// DOM Form Elements
     const taskInput = document.getElementById("task-input");
     const dueDateInput = document.getElementById("due-date-input");
 
-    /// Validate Inputs
     if (taskInput.value === "" || dueDateInput.value === "") {
         alert("Task cannot be empty.");
         return false;
@@ -23,16 +23,19 @@ function addTodo(task, dueDate) {
     });
 
     renderList()
-    ///console.log("Task added:", task);
 }
 
 function renderList() {
     const taskList = document.getElementById("task-list");
     taskList.innerHTML = ""; // Clear existing list
 
-    
+    for (let i =0; i < listTodo.length; i++) {
+        taskList.innerHTML += `<li class="border-b py-2">${listTodo[i].task} - Due: ${listTodo[i].dueDate}</li>`;
+    }
 }
 
+/// Delete all Todos
 function deleteAll() {
-    
+    listTodo = [];
+    renderList();
 }
